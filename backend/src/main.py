22822b2e0d2dfc693887a84a869b1f0a369cc892
ines-sanluis <sanluis.tfg@gmail.com>
2019-03-03 15:41:33 +0100

    Recibir imaxe no backend

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,6 +2,8 @@ from .entidades.entidade import Session, engine, Base
 from .entidades.analise import Analise, AnaliseSchema
 from flask_cors import CORS
 from flask import Flask, jsonify, request
+import cv2
+import numpy as np
 
 print("Backend Running")
 
@@ -44,3 +46,22 @@ def getMelanoma():
     data = schema.dump(data_objects)
     session.close()
     return jsonify(data.data)
+
+@app.route('/file-upload', methods=['POST'])
+def imageUpload():
+    # print(request.files)
+    if 'image' not in request.files:
+        print('Erro: non hai petición de imaxe')
+        return jsonify("Non recibido")
+    file = request.files['image']
+    if file.filename == '':
+        print("Erro: non hai un arquivo seleccionado")
+        return jsonify("Non recibido")
+    print(file)
+    filestr = request.files['image'].read()
+    npimg = np.fromstring(filestr, np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
+    # cv2.imshow('Imaxe', img) #titulo da imaxe e fonte
+    # cv2.waitKey(0) #esperar a que o usuario pulse unha tecla
+    # cv2.destroyAllWindows() #pechar a venta
+    return jsonify("Recibido")
