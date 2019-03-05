7e11719b3fc7de64a363cf8813e085b324ceabb9
ines-sanluis <sanluis.tfg@gmail.com>
2019-03-05 13:30:06 +0100

    Reducir codigo para obter Ph2Dataset

--- backend/src/main.py
+++ backend/src/main.py
@@ -6,42 +6,20 @@ import cv2
 import numpy as np
 
 print("Backend Running")
-
+img = 'NULL'
 app = Flask(__name__)
 CORS(app)
 Base.metadata.create_all(engine)
 
-@app.route('/ph2dataset')
-def getPh2Dataset():
+@app.route('/ph2dataset/<diagnose>')
+def getPh2Dataset(diagnose):
     session = Session()
-    data_objects = session.query(Analise).all()
-    schema = AnaliseSchema(many=True)
-    data = schema.dump(data_objects)
-    session.close()
-    return jsonify(data.data)
-
-@app.route('/common_nevus')
-def getCommonNevus():
-    session = Session()
-    data_objects = session.query(Analise).filter(Analise.clinical_diagnosis == 'Common Nevus')
-    schema = AnaliseSchema(many=True)
-    data = schema.dump(data_objects)
-    session.close()
-    return jsonify(data.data)
-
-@app.route('/atypical_nevus')
-def getAtypicalNevus():
-    session = Session()
-    data_objects = session.query(Analise).filter(Analise.clinical_diagnosis == 'Atypical Nevus')
-    schema = AnaliseSchema(many=True)
-    data = schema.dump(data_objects)
-    session.close()
-    return jsonify(data.data)
-
-@app.route('/melanoma')
-def getMelanoma():
-    session = Session()
-    data_objects = session.query(Analise).filter(Analise.clinical_diagnosis == 'Melanoma')
+    if diagnose == "todas": data_objects = session.query(Analise).all()
+    else:
+        if diagnose == "common_nevus": d = "Common Nevus"
+        elif diagnose == "atypical_nevus": d = "Atypical Nevus"
+        elif diagnose == "melanoma": d = "Melanoma"
+        data_objects = session.query(Analise).filter(Analise.clinical_diagnosis == d)
     schema = AnaliseSchema(many=True)
     data = schema.dump(data_objects)
     session.close()
